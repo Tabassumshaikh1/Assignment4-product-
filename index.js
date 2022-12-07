a64c0f9bd71ee858dd0b94a17336fff36867c7bd
Tabassumshaikh1 <114898782+Tabassumshaikh1@users.noreply.github.com>
2022-12-07 17:48:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,18 +7,23 @@ app.set("view engine","ejs")
 app.set("views",'./views')
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-//all routes can access
+//all routes cann
 const mainRoutes=require('./app')
 app.use('/',mainRoutes);
 // const connection = connect()
-const DB = mongoose.connect('mongodb+srv://Tabassum:Tabassum1234@cluster0.f8r4yqy.mongodb.net/myProducts',{useNewUrlParser:true,useUnifiedTopology:true},(err)=>{
+const DB = mongoose.connect('mongodb+srv://Tabassum:Tabassum1234@cluster0.f8r4yqy.mongodb.net/?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true},(err)=>{
     if(err){
+        console.log("Tabassum")
         console.log(err);   
     } 
     else{
         console.log(`database is connected successfully`);
     }
-})
+});
+
+//  mongoose.connection();
+// DB.on("error", () => console.log("error"))
+// DB.once("open", () => console.log("Connected"))
 
 app.listen(PORT,(err)=>{
     console.log(`Server is working on ${PORT}`);
